1675b506cb816d3456ec17b46c17fc449c0c4b4e
Nathan Oertel <nathan@digitalclouddesigns.com>
2015-02-15 21:22:14 -0500

    Removed unnecessary classes, simplified styling and added language support

---
+++ modules/servers/opensrsemail/lang/english.php
@@ -0,0 +1,74 @@
+<?php
+// Mailbox list page
+$lang["mailboxes"]["logintowebmail"] = "Login To Webmail";
+$lang["mailboxes"]["setupinstructions"] = "Download Setup Instructions";
+$lang["mailboxes"]["addmailbox"] = "Add Mailbox";
+$lang["mailboxes"]["addforward"] = "Add Forwarding Mailbox";
+$lang["mailboxes"]["addalias"] = "Add Alias";
+$lang["mailboxes"]["editworkgroups"] = "Edit Workgroups";
+$lang["mailboxes"]["mailboxaddsuccess"] = "The mailbox has been added successfully.";
+$lang["mailboxes"]["mailboxeditsuccess"] = "The mailbox has been updated successfully.";
+$lang["mailboxes"]["mailboxdeletesuccess"] = "The mailbox has been deleted successfully.";
+$lang["mailboxes"]["mailboxdeleterequired"] = 'You have more than the allowed number of mailboxes, either <a href="upgrade.php?type=configoptions&id={$serviceid}">add more mailboxes</a> to your account or remove mailboxes.';
+$lang["mailboxes"]["forwarddeleterequired"] = 'You have more than the allowed number of forwards, either <a href="upgrade.php?type=configoptions&id={$serviceid}">add more forwards</a> to your account or remove forward mailboxes.';
+$lang["mailboxes"]["name"] = "Name";
+$lang["mailboxes"]["type"] = "Type";
+$lang["mailboxes"]["workgroup"] = "Workgroup";
+$lang["mailboxes"]["edit"] = "Edit";
+$lang["mailboxes"]["delete"] = "Delete";
+$lang["mailboxes"]["nomailboxes"] = "There are no mailboxes.";
+
+// Mailbox add/edit page
+$lang["mailbox"]["addmailbox"] = "Add Mailbox";
+$lang["mailbox"]["editmailbox"] = "Edit Mailbox";
+$lang["mailbox"]["deleterequired"] = 'You have exceeded the number of mailboxes you are allowed to have, please <a href="upgrade.php?type=configoptions&id={$serviceid}">add more mailboxes</a> in order to continue.';
+$lang["mailbox"]["mailboxinfo"] = "Mailbox Info";
+$lang["mailbox"]["mailboxname"] = "Mailbox Name";
+$lang["mailbox"]["password"] = "Password";
+$lang["mailbox"]["blankfornochange"] = "(leave blank for no change)";
+$lang["mailbox"]["confirmpassword"] = "Confirm Password";
+$lang["mailbox"]["workgroup"] = "Workgroup";
+$lang["mailbox"]["userinfo"] = "User Info";
+$lang["mailbox"]["title"] = "Title";
+$lang["mailbox"]["firstname"] = "First Name";
+$lang["mailbox"]["lastname"] = "Last Name";
+$lang["mailbox"]["phone"] = "Phone";
+$lang["mailbox"]["fax"] = "Fax";
+$lang["mailbox"]["save"] = "Save";
+$lang["mailbox"]["cancel"] = "Cancel";
+
+// Forward add/edit page
+$lang["forward"]["addforward"] = "Add Forwarding Mailbox";
+$lang["forward"]["editforward"] = "Edit Forwarding Mailbox";
+$lang["forward"]["deleterequired"] = 'You have exceeded the number of mailboxe forwards you are allowed to have, please <a href="upgrade.php?type=configoptions&id={$serviceid}">add more forwards</a> in order to continue.';
+$lang["forward"]["mailboxname"] = "Mailbox Name";
+$lang["forward"]["forwardto"] = "Forward To (comma separated email addresses)";
+$lang["forward"]["workgroup"] = "Workgroup";
+$lang["forward"]["save"] = "Save";
+$lang["forward"]["cancel"] = "Cancel";
+
+// Alias add page
+$lang["alias"]["addalias"] = "Add Alias";
+$lang["alias"]["aliasname"] = "Alias";
+$lang["alias"]["mailboxname"] = "To Mailbox";
+$lang["alias"]["save"] = "Save";
+$lang["alias"]["cancel"] = "Cancel";
+
+// Workgroup list page
+$lang["workgroups"]["addworkgroup"] = "Add Workgroup";
+$lang["workgroups"]["editmailboxes"] = "Edit Mailboxes";
+$lang["workgroups"]["workgroupaddsuccess"] = "The workgroup was added successfully.";
+$lang["workgroups"]["workgroupdeletesuccess"] = "The workgroup was deleted successfully.";
+$lang["workgroups"]["name"] = "Name";
+$lang["workgroups"]["mailboxes"] = "Mailboxes";
+$lang["workgroups"]["forwards"] = "Forwards";
+$lang["workgroups"]["aliases"] = "Aliases";
+$lang["workgroups"]["delete"] = "Delete";
+$lang["workgroups"]["noworkgroups"] = "There are no workgroups.";
+
+// Workgroup add/edit page
+$lang["workgroup"]["addworkgroup"] = "Add Workgroup";
+$lang["workgroup"]["workgroup"] = "Workgroup";
+$lang["workgroup"]["save"] = "Save";
+$lang["workgroup"]["cancel"] = "Cancel";
+?>
